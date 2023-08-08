c02b1df5e4c6926f9bce0f8ed76f8ae708fac728
joko <joko@neuronworks.co.id>
2023-08-08 14:27:07 +0700

    penambahan module restapi_door untuk api get content

---
+++ web/modules/restapi_door/src/Controller/ContentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\restapi_door\Controller;
+
+use Drupal;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContentController extends ControllerBase{
+    public function detail($content_id, Request $request)
+    {   
+
+        if (empty($content_id)) {
+            return Drupal::service('restapi_door.app_helper')->response([
+               'status'  => 'failed',
+               'info' => 'landing page id cannot be empty',
+               'data'    => []
+            ], 400);
+        };
+   
+        $result = [];
+        $fileHelper = Drupal::service('file_url_generator');
+        $entity     = Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['uuid' => $content_id]);
+   
+        if (!empty($entity)) :
+            $entity_obj = current($entity);
+            $imageData  = $entity_obj->getType() === 'article' ? $entity_obj->get('field_image')->referencedEntities() : $entity_obj->get('field_news_image')->referencedEntities();
+
+            $result = array(
+                'uuid' => $entity_obj->uuid(),
+                'name' => $entity_obj->label(),
+                'lang' => $entity_obj->langcode->getString(),
+                'module' => $entity_obj->getType(),
+                'content_body'  => $entity_obj->body->getValue(),
+                'content_image' => array_map(function($res) use ($fileHelper) {
+                    $imageLink = $res->uri->getString();
+
+                    if (str_contains($imageLink, 's3')) {
+                        $findS3 = Drupal::service('restapi_telkom.minio_helper')->getFileByURI($imageLink, 'original');
+
+                        $displayImg = $findS3['status'] ? $findS3['data'] : $imageLink;
+                    }
+                    else {
+                        $displayImg = Drupal::request()->getSchemeAndHttpHost() . $fileHelper->generateString($imageLink);
+                    };
+
+                    return [
+                        'uuid' => $res->uuid(),
+                        'filename' => $res->filename->getString(),
+                        'mimeType' => $res->filemime->getString(),
+                        'location' => $displayImg
+                    ];
+                }, $imageData),
+                'status'       => $entity_obj->isPublished() ? 'published' : 'hidden',
+                'created_date' => date("Y-m-d H:i:s", $entity_obj->getCreatedTime()),
+                'last_update'  => date("Y-m-d H:i:s", $entity_obj->getChangedTime())
+            );
+        endif;
+   
+        return Drupal::service('restapi_door.app_helper')->response([
+            'status'  => !empty($result) ? 'success' : 'failed',
+            'info' => !empty($result) ? 'success to retrieve data' : 'theres no data related to selected paket id',
+            'data'    => $result
+        ]);
+
+    }
+
+}
