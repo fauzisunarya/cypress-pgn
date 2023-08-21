fa82b9c7130515347cb00390648d5678eb7d60f9
rijky <rijky.sallaffudin@neuronworks.co.id>
2023-08-21 14:21:44 +0700

    perbaikan unit tes yang salah

--- web/modules/restapi_door/tests/src/Unit/Controller/GetContentControllerTest.php
+++ web/modules/restapi_door/tests/src/Unit/Controller/GetContentControllerTest.php
@@ -2,46 +2,88 @@
 
 namespace Drupal\restapi_door\Tests\Unit\Controller;
 
-use Drupal\Tests\Core\Menu\LocalTaskIntegrationTestBase;
+use Drupal\Tests\UnitTestCase; 
+use Drupal\restapi_door\Controller\ContentController; 
+use Symfony\Component\HttpFoundation\Request; 
+use Symfony\Component\HttpClient\HttpClient; 
 
 
-class ContentControllerTest extends LocalTaskIntegrationTestBase
+class ContentControllerTest extends UnitTestCase
 {
 
-  public function testGetContentApi()
-  {
-    $url = 'apidoor/contents/311758db-375b-4f61-b58f-0e9e19b923a6';
+  // public function testBerhasilMemunculkanDataContent()
+  // {
+  //   $url = 'apidoor/contents/311758db-375b-4f61-b58f-0e9e19b923a6';
 
-    // Initialize cURL session
-    $ch = curl_init($url);
+  //   // Initialize cURL session
+  //   $ch = curl_init($url);
 
-    $headers = [
-      'Authorization: '.'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWQiOiJjaHRnY2o3OG9rN29ib3ZiMm5tb2hjM2ZtZCIsInN1YiI6IjgiLCJleHAiOjE5MjQ3OTQwMDAsImNvZGUiOiIyMDIzMDAwMDAxIiwibmFtZSI6IlNhbXBsZSBFbXBsb3llZSIsImVtYWlsIjoiZW1wQGRvb3J2My5jb20iLCJhcHBpZCI6OCwiYXBwbmFtZSI6ImRvb3IiLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiZW1wbG95ZWUiXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiZW1wbG95ZWUifSwiZmxhZyI6MSwidXJsIjoiaHR0cHM6Ly91YW0tZGV2Lm5ldXJvbi5pZCIsImNvbmZpZyI6IntcImFwcGxpY2F0aW9uXCI6e1wibmFtZVwiOlwiZG9vclwiLFwiY29tcGFueVwiOlwiZG9vciBuZXVyb25cIixcImljb25cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29fbmFtZVwiOlwiaHR0cHM6Ly9ocm1pcy5uZXVyb24uaWQvYXNzZXRzL3BlcnNvbmFsaXphdGlvbi83ZTk4ZDRiMjIwMDBkYjg0YWQxNjczMTExNDBlZTZiMS5wbmdcIixcImltYWdlX2xvYWRlclwiOlwiaHR0cHM6Ly9jZG4ubmV1cm9ud29ya3MuY28uaWQvZG9vcnYzL2ltYWdlcy9mYXZpY29uLmljb1wiXHJcbn0sXCJlbXBsb3llZVwiOntcImFwcGxpY2F0aW9uXCI6XCJkb29yLGRvb3Jtb2JpbGVcIn19Iiwic2FsdCI6IjcxYjAwY2ExNDZmYjY0YjNiMmNiN2MwN2U0NmNmZWRlNDYxMzg4YTZiYjY3MTJmZjkxMjZlYzhlNjE4NjkwMjIiLCJhbGxvd2VkX3JvbGVzIjpbImVtcGxveWVlIl19.qwQC0n5Y7SFQ2gUzm5XOA9gIcFVShG1AuvOdyZtHSg0Qht4Hdc-ip-ugycoi2R2pT4gblpJwK5ey3SAWXNJbySlyPvaA4TyR7RwrZ28SmCvToGqStg6WJWca0kAL8m7IG6aDygnH9T_0qFQ1n0lr33oPzCafgkTH-Exe98PBTak'
-    ];
+  //   $headers = [
+  //     'Authorization: '.'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWQiOiJjaHRnY2o3OG9rN29ib3ZiMm5tb2hjM2ZtZCIsInN1YiI6IjgiLCJleHAiOjE5MjQ3OTQwMDAsImNvZGUiOiIyMDIzMDAwMDAxIiwibmFtZSI6IlNhbXBsZSBFbXBsb3llZSIsImVtYWlsIjoiZW1wQGRvb3J2My5jb20iLCJhcHBpZCI6OCwiYXBwbmFtZSI6ImRvb3IiLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiZW1wbG95ZWUiXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiZW1wbG95ZWUifSwiZmxhZyI6MSwidXJsIjoiaHR0cHM6Ly91YW0tZGV2Lm5ldXJvbi5pZCIsImNvbmZpZyI6IntcImFwcGxpY2F0aW9uXCI6e1wibmFtZVwiOlwiZG9vclwiLFwiY29tcGFueVwiOlwiZG9vciBuZXVyb25cIixcImljb25cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29fbmFtZVwiOlwiaHR0cHM6Ly9ocm1pcy5uZXVyb24uaWQvYXNzZXRzL3BlcnNvbmFsaXphdGlvbi83ZTk4ZDRiMjIwMDBkYjg0YWQxNjczMTExNDBlZTZiMS5wbmdcIixcImltYWdlX2xvYWRlclwiOlwiaHR0cHM6Ly9jZG4ubmV1cm9ud29ya3MuY28uaWQvZG9vcnYzL2ltYWdlcy9mYXZpY29uLmljb1wiXHJcbn0sXCJlbXBsb3llZVwiOntcImFwcGxpY2F0aW9uXCI6XCJkb29yLGRvb3Jtb2JpbGVcIn19Iiwic2FsdCI6IjcxYjAwY2ExNDZmYjY0YjNiMmNiN2MwN2U0NmNmZWRlNDYxMzg4YTZiYjY3MTJmZjkxMjZlYzhlNjE4NjkwMjIiLCJhbGxvd2VkX3JvbGVzIjpbImVtcGxveWVlIl19.qwQC0n5Y7SFQ2gUzm5XOA9gIcFVShG1AuvOdyZtHSg0Qht4Hdc-ip-ugycoi2R2pT4gblpJwK5ey3SAWXNJbySlyPvaA4TyR7RwrZ28SmCvToGqStg6WJWca0kAL8m7IG6aDygnH9T_0qFQ1n0lr33oPzCafgkTH-Exe98PBTak'
+  //   ];
 
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, $headers);
+  //   // Set cURL options
+  //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  //   curl_setopt($ch, CURLOPT_HEADER, $headers);
 
-    // Execute cURL session and get response
-    // $response = curl_exec($ch);
+  //   // Get HTTP status code
+  //   $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-    // Get HTTP status code
-    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  //   // Close cURL session
+  //   curl_close($ch);
 
-    // Close cURL session
-    curl_close($ch);
+  //   // Assert HTTP status code is 200 (OK)
+  //   $this->assertEquals($code, 0);
 
-    // Assert HTTP status code is 200 (OK)
-    $this->assertEquals($code, 0);
+  // }
 
-    // // Parse JSON response
-    // $content = json_decode($response, true);
+  public function testGetContentApi()  
+  {  
+  
+    /** CARA 2 */  
+  
+    $url = 'https://content-management-service.test/apidoor/contents/311758db-375b-4f61-b58f-0e9e19b923a6';  
+  
+    $headers = [  
+      'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWQiOiJjaHRnY2o3OG9rN29ib3ZiMm5tb2hjM2ZtZCIsInN1YiI6IjgiLCJleHAiOjE5MjQ3OTQwMDAsImNvZGUiOiIyMDIzMDAwMDAxIiwibmFtZSI6IlNhbXBsZSBFbXBsb3llZSIsImVtYWlsIjoiZW1wQGRvb3J2My5jb20iLCJhcHBpZCI6OCwiYXBwbmFtZSI6ImRvb3IiLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiZW1wbG95ZWUiXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiZW1wbG95ZWUifSwiZmxhZyI6MSwidXJsIjoiaHR0cHM6Ly91YW0tZGV2Lm5ldXJvbi5pZCIsImNvbmZpZyI6IntcImFwcGxpY2F0aW9uXCI6e1wibmFtZVwiOlwiZG9vclwiLFwiY29tcGFueVwiOlwiZG9vciBuZXVyb25cIixcImljb25cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29cIjpcImh0dHBzOi8vY2RuLm5ldXJvbndvcmtzLmNvLmlkL2Rvb3J2My9pbWFnZXMvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29fbmFtZVwiOlwiaHR0cHM6Ly9ocm1pcy5uZXVyb24uaWQvYXNzZXRzL3BlcnNvbmFsaXphdGlvbi83ZTk4ZDRiMjIwMDBkYjg0YWQxNjczMTExNDBlZTZiMS5wbmdcIixcImltYWdlX2xvYWRlclwiOlwiaHR0cHM6Ly9jZG4ubmV1cm9ud29ya3MuY28uaWQvZG9vcnYzL2ltYWdlcy9mYXZpY29uLmljb1wiXHJcbn0sXCJlbXBsb3llZVwiOntcImFwcGxpY2F0aW9uXCI6XCJkb29yLGRvb3Jtb2JpbGVcIn19Iiwic2FsdCI6IjcxYjAwY2ExNDZmYjY0YjNiMmNiN2MwN2U0NmNmZWRlNDYxMzg4YTZiYjY3MTJmZjkxMjZlYzhlNjE4NjkwMjIiLCJhbGxvd2VkX3JvbGVzIjpbImVtcGxveWVlIl19.qwQC0n5Y7SFQ2gUzm5XOA9gIcFVShG1AuvOdyZtHSg0Qht4Hdc-ip-ugycoi2R2pT4gblpJwK5ey3SAWXNJbySlyPvaA4TyR7RwrZ28SmCvToGqStg6WJWca0kAL8m7IG6aDygnH9T_0qFQ1n0lr33oPzCafgkTH-Exe98PBTak'  
+    ];  
+  
+    $client = HttpClient::create(['verify_peer' => false, 'verify_host' => false]); 
+    $response = $client->request(  
+      'GET',  
+      $url,  
+      [  
+        'headers' => $headers  
+      ]  
+    );  
+    var_dump($response);  
+  }  
 
-    // Assert JSON response is valid and contains expected keys
-    // $this->assertIsArray($content);
-    // $this->assertArrayHasKey('id', $content);
-    // $this->assertArrayHasKey('title', $content);
-    // $this->assertArrayHasKey('body', $content);
-  }
+
+  // public function testgagalMemunculkanDataContentDenganInformasiJwtInvalid()
+  // {
+  //   $url = 'apidoor/contents/311758db-375b-4f61-b58f-0e9e19b923a6';
+
+  //   // Initialize cURL session
+  //   $ch = curl_init($url);
+
+  //   $headers = [
+  //     'Authorization: '.'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9'
+  //   ];
+
+  //   // Set cURL options
+  //   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  //   curl_setopt($ch, CURLOPT_HEADER, $headers);
+
+  //   // Get HTTP status code
+  //   $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+  //   // Close cURL session
+  //   curl_close($ch);
+
+  //   // Assert HTTP status code is 200 (OK)
+  //   $this->assertNotEquals($code, 0);
+  // }
+
+
 }
---
+++ web/modules/restapi_door/tests/src/Unit/Controller/UpdateContentControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\restapi_door\Tests\Unit\Controller;
+
+use Drupal\Tests\Core\Menu\LocalTaskIntegrationTestBase;
+use Drupal\restapi_door\Controller\ContentController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
+
+class UpdateContentControllerTest extends LocalTaskIntegrationTestBase
+{
+    public function testUpdateContentApi()
+    {
+        $url = 'https://content-management-service.test/apidoor/contents/update';
+
+        $headers = [
+            'Authorization' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWQiOiJidTFlNTFvbjhzYm9tYTRocG1rNjc1MWFlcCIsInN1YiI6IjMiLCJleHAiOjE3OTAzNDc3NjEsImNvZGUiOiIxMDAxODA0MTkxIiwibmFtZSI6InVuaXRcclxuZW1wIiwiZW1haWwiOiJ1bml0LnRlc3QuZW1wQG5ldXJvbndvcmtzLmNvLmlkIiwibGFuZyI6ImlkIiwiYXBwaWQiOjgsImFwcG5hbWUiOiJkb29yIiwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbImVtcGxveWVlIl0sIngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6ImVtcGxveWVlIn0sImZsYWciOjEsInVybCI6Imh0dHBzOlxcL1xcL3VhbS1kZXYubmV1cm9uLmlkIiwiY29uZmlnIjoie1wiYXBwbGljYXRpb25cIjphcnJheShcIm5hbWVcIjpcImRvb3JcIixcImNvbXBhbnlcIjpcImRvb3JcclxubmV1cm9uXCIsXCJpY29uXCI6XCJodHRwczpcXC9cXC9jZG4ubmV1cm9ud29ya3MuY28uaWRcXC9kb29ydjNcXC9pbWFnZXNcXC9mYXZpY29uLmljb1wiLFwiaW1hZ2VfbG9nb1wiOlwiaHR0cHM6XFwvXFwvY2RuLm5ldXJvbndvcmtzLmNvLmlkXFwvZG9vcnYzXFwvaW1hZ2VzXFwvZmF2aWNvbi5pY29cIixcImltYWdlX2xvZ29fbmFtZVwiOlwiaHR0cHM6XFwvXFwvY2RuLm5ldXJvbndvcmtzLmNvLmlkXFwvZG9vcnYzXFwvaW1hZ2VzXFwvaG9yaXpvbnRhbC1sb2dvLnBuZ1wiLFwiaW1hZ2VfbG9hZGVyXCI6XCJodHRwczpcXC9cXC9jZG4ubmV1cm9ud29ya3MuY28uaWRcXC9kb29ydjNcXC9pbWFnZXNcXC9mYXZpY29uLmljb1wiXHJcbiksXCJlbXBsb3llZVwiOmFycmF5KFwiYXBwbGljYXRpb25cIjpcImRvb3IsZG9vcm1vYmlsZVwiKSxcImF0dGVuZGFuY2VcIjphcnJheShcImN1dG9mZlwiOlxyXG5cIjI2XCIpfSIsInNhbHQiOiJhZWE5N2Q1YmI3Njc0NWY0ZTFlNzA4ZWFmMWFhYjczN2M5Mzc5N2E0Yjk0NWE0NGM5ZmZiMzczNzYxN2NlZWYyIiwiYWxsb3dlZF9yb2xlcyI6WyJlbXBsb3llZSJdfQ.Ce07InGDJFatQaCWJHPRYmv6xGrCLOI6DhIW7egODe1Rv-Vdf4pXR0ARn-Dy04HDZSI_YD-kJP8JK0qbaKtt_H8qOBsGVOca1vQW1yJ7ulzWlFDO6CxODEwwSfLfoGvod7gkwTUfnHYERwpeZ2kH_Uw9ZWhtLzpjRuhvsEwISiU'
+        ];
+
+        $body = [
+            "data" => [
+                "uuid" => "84d90d4b-f6c9-459f-b98e-68a02779bea8",
+                "name" => "background for training edit",
+                "lang" => "en",
+                "module" => "article",
+                "content_body" => [
+                    [
+                        "value" => "<h4>[edit edit] please use a background like the image below</h4> <br> <img src=\"https://content-management-service.test/apidoor/contents/image?id=uploads/cms/2023/08/14/training.jpg\"> <br> <h4> while participating in the training event </h4>",
+                        "summary" => "",
+                        "format" => "basic_html"
+                    ]
+                ],
+                "status" => "published",
+                "created_date" => "2023-08-09 15:20:43",
+                "last_update" => "2023-08-09 20:17:11"
+            ]
+        ];
+
+        // Create HTTP client
+        $client = HttpClient::create(['verify_peer' => false, 'verify_host' => false]);
+
+        // Send POST request
+        $response = $client->request(
+            'POST',
+            $url,
+            [
+                'headers' => $headers,
+                'json' => $body,
+            ]
+        );
+
+        // Parse JSON response
+        $content = $response->toArray();
+        $this->assertEquals($content['code'], 0);
+    }
+}
