25b3eb5add44b73e4b7edf1fab07a88af362dd0a
Muhammad Ikram Zasbita <ikram@neuronworks.co.id>
2023-11-22 14:55:58 +0700

    push

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,22 +5,6 @@ import ContentList from 'src/Pages/Content/List';
 
 export default function Router() {
   return useRoutes([
-    {
-      path: '/',
-      children: [
-        { path: 'content-list', element: <ContentList /> },
-      ],
-    },
-    {
-      element: <CompactLayout />,
-      children: [{ path: '404', element: <Page404 /> }],
-    },
-    {
-      path: '/iframe-example',
-      element: (
-        <IframeExample />
-      )
-    },
     { path: '*', element: <Navigate to="/404" replace /> },
   ]);
 }
