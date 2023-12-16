e3ac1287e6b52257f59c5f803e566b943407a3b2
Muhammad Ikram Zasbita <ikram@neuronworks.co.id>
2023-12-16 12:25:28 +0700

    fix error

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -318,7 +318,7 @@ class ContentController extends Controller {
                         $img_banner = '';
                         if (isset($val['image_banner'])) {
                             if(strlen($val['image_banner']) <=3){
-                                $img_banner = $row['image_banner'];
+                                $img_banner = $val['image_banner'];
                             }else{
                                 $img_banner = $val['image_banner'];
                                 if (!filter_var($val['image_banner'], FILTER_VALIDATE_URL)) {
